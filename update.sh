f561dd7aeaa8924001a143869acce3e2b91a5277
Joey Innes <joey@inneslabs.uk>
2024-05-08 18:07:47 +0200

    Yes

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 while IFS= read -r host
 do
     echo "UPDATING HOST $host"
-    ssh -n -o StrictHostKeyChecking=no admin@$host "sudo rm daved daved_edge.service.conf"
-    scp -o StrictHostKeyChecking=no daved daved_edge.service.conf admin@$host:~
-    ssh -n -o StrictHostKeyChecking=no admin@$host "sudo cp -f daved_edge.service.conf /etc/systemd/system/daved.service && sudo systemctl daemon-reload && sudo systemctl enable daved && sudo systemctl restart daved && sudo systemctl status daved"
-    sleep 2 # So they don't start in lock-step.
+    ssh -i ~/.ssh/awsintob.pem -n -o StrictHostKeyChecking=no admin@$host "sudo rm daved daved_edge.service.conf"
+    scp -i ~/.ssh/awsintob.pem -o StrictHostKeyChecking=no daved daved_edge.service.conf admin@$host:~
+    ssh -i ~/.ssh/awsintob.pem -n -o StrictHostKeyChecking=no admin@$host "sudo cp -f daved_edge.service.conf /etc/systemd/system/daved.service && sudo systemctl daemon-reload && sudo systemctl enable daved && sudo systemctl restart daved && sudo systemctl status daved"
+    sleep 1 # So they don't start in lock-step.
 done < "hostnames_edges"
