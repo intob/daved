64429fce1bbc7702d974e6bf4b7dbf3d33c40e86
Joey Innes <joey@inneslabs.uk>
2024-06-06 20:15:20 +0100

    Various improvements

--- intob_roll.sh
+++ intob_roll.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 ./intob_update.sh admin edge1 intob_edge.service.conf
-./intob_update.sh joey pi1.local intob_peer.service.conf
+./intob_update.sh joey pi1.local intob_pi1.service.conf
+./intob_update.sh joey pi2.local intob_pi2.service.conf
 ./intob_update.sh admin aws1 intob_peer.service.conf
 ./intob_update.sh admin aws2 intob_peer.service.conf
-./intob_update.sh admin aws3 intob_peer.service.conf
-./intob_update.sh admin aws4 intob_peer.service.conf
-./intob_update.sh admin aws5 intob_peer.service.conf
-./intob_update.sh admin aws6 intob_peer.service.conf
